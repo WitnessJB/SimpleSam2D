754a9120108ec11723ac1e48816cfff5c6b2f495
WitnessJB <167652375+WitnessJB@users.noreply.github.com>
2025-03-28 21:01:21 -0400

    Added Assets File
    Grouped assets into a separate file

---
+++ assets.py
@@ -0,0 +1,7 @@
+import pygame
+import os
+
+houseImg = pygame.image.load('SimpleHouse.png')
+hikerJeremy = pygame.image.load('Jeremy_Hiking.png')
+defaultSam = pygame.image.load('Sam_Standing.png')
+mRoad = pygame.image.load('MountainRoad.png')
